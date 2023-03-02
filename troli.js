a005a1eb65f51236c093d8a366dd61787b60e5fa
Szabó Dániel <szdanika12345@gmail.com>
2023-03-02 12:51:57 +0100

    troli

--- troli.js
+++ troli.js
@@ -12,16 +12,17 @@ vegallomasNevek["79"] = "Keleti pályaudvar - Kárpát utca";
 vegallomasNevek["80"] = "Keleti pályaudvar - Örs vezér tere";
 vegallomasNevek["81"] = "Örs vezér tere - Fischer István utca";
 vegallomasNevek["82"] = "Uzsoki Utcai Kórház - Örs vezér tere";
+vegallomasNevek["83"] = "Fővám tér - Orczy tér";
 
 function jaratKiiras(elem, index){
 	var ujElem = document.createElement("option");
     var ujSzoveg = document.createTextNode(index); 
     ujElem.appendChild(ujSzoveg);
-    document.getElementById("jaratSzam").appendChild(ujElem);
+    document.getElementById("jaratSzam").appendChild(ujElem);	
 }
 
 function vegallomasKiiras(jarat){
-    document.getElementById("valasz").innerHTML=vegallomasNevek[jarat];
+    document.getElementById("vegallomasok").innerHTML=vegallomasNevek[jarat];
 }
 
 function fel() {
